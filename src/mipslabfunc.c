1f5c2add37770f03c680d82d6d1234dd99687d0d
Sareh <82710940+Sarehj@users.noreply.github.com>
2023-02-27 00:22:23 +0100

    Update mipslabfunc.c

--- src/mipslabfunc.c
+++ src/mipslabfunc.c
@@ -1,8 +1,4 @@
-/* mipslabfunc.c
-   This file written 2015 by F Lundevall
-   Some parts are original code written by Axel Isaksson
 
-   For copyright and licensing, see file COPYING */
 
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
