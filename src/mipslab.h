aec3413a447800d5d5ff6baf8b8589ac2f15b673
Sareh <82710940+Sarehj@users.noreply.github.com>
2023-02-27 00:21:49 +0100

    Update mipslab.h

--- src/mipslab.h
+++ src/mipslab.h
@@ -1,11 +1,4 @@
-/* mipslab.h
-   Header file for all labs.
-   This file written 2015 by F Lundevall
-   Some parts are original code written by Axel Isaksson
 
-   Latest update 2015-08-28 by F Lundevall
-
-   For copyright and licensing, see file COPYING */
 
 	 #pragma once
 
